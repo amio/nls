a473988664209bf1ded26832196dc9e8ca7432ce
Amio <amio.cn@gmail.com>
2023-01-14 00:43:48 +0800

    chore: bump dependencies, requires nodejs 16 at minimal

---
+++ tap-snapshots/test/cli.spec.js.test.cjs
@@ -0,0 +1,24 @@
+/* IMPORTANT
+ * This snapshot file is auto-generated, but designed for humans.
+ * It should be checked into source control and tracked carefully.
+ * Re-generate by setting TAP_SNAPSHOT=1 and running tests.
+ * Make sure to inspect the output below.  Do not ignore changes!
+ */
+'use strict'
+exports[`test/cli.spec.js TAP runs \`why qs\` > snapshot 1`] = `
+
+  No one requires qs.
+`
+
+exports[`test/cli.spec.js TAP runs in root dir > snapshot 1`] = `
+
+  npm scripts:
+
+⁣ * lint    standard lib/*                                                                 
+⁣ * pretest npm run lint                                                                   
+⁣ * test    tap test/*.spec.js --reporter spec --test-env=FORCE_COLOR=0 --no-check-coverage
+⁣ * snap    TAP_SNAPSHOT=1 npm run test                                                    
+⁣ * build   ncc build lib/nls -m                                                           
+⁣ * prepack npm run build                                                                   
+
+`
