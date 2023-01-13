7578d2098f6d3bc16ff036f774a4d15cc3cef7f5
Amio <amio.cn@gmail.com>
2023-01-14 00:55:53 +0800

    chore: remove build step

--- tap-snapshots/test/cli.spec.js.test.cjs
+++ tap-snapshots/test/cli.spec.js.test.cjs
@@ -17,8 +17,6 @@ exports[`test/cli.spec.js TAP runs in root dir > snapshot 1`] = `
 ⁣ * lint    standard lib/*                                                                 
 ⁣ * pretest npm run lint                                                                   
 ⁣ * test    tap test/*.spec.js --reporter spec --test-env=FORCE_COLOR=0 --no-check-coverage
-⁣ * snap    TAP_SNAPSHOT=1 npm run test                                                    
-⁣ * build   ncc build lib/nls -m                                                           
-⁣ * prepack npm run build                                                                   
+⁣ * snap    TAP_SNAPSHOT=1 npm run test                                                     
 
 `
